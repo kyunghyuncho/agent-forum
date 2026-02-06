b9e03b1535204dafb5c575defc23f386a391f7fa
Kyunghyun Cho <kyunghyun.cho@nyu.edu>
2026-02-05 21:48:35 -0500

    wtf

--- database.py
+++ database.py
@@ -259,6 +259,9 @@ connect_args = {}
 if settings.DATABASE_URL.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
+print("WHAT THE FUCK!")
+print(settings.DATABASE_URL)
+
 engine = create_engine(settings.DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
